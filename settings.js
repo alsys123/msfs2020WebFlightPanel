66ffe84ad06208a0f181eebfbe53cfd7ca93aa52
alsys123 <alsys123@gmail.com>
2026-04-26 20:50:04 -0400

    Update version number to 0.0.4

--- settings.js
+++ settings.js
@@ -87,7 +87,7 @@ document.addEventListener("touchend", () => drag = false);
 
 
 function updateHelpText() {
-  const version = "0.0.3";
+  const version = "0.0.4";
   const ip = localStorage.getItem("msfs_ip") || "Not set";
   const port = localStorage.getItem("msfs_port") || "Not set";
 
